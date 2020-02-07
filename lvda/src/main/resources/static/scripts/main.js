f142b47999ab5f77a2da701627b0866b2ca01c17
longhsu91 <longhsu91@gmail.com>
2020-02-07 10:23:32 +0800

    add

--- lvda/src/main/resources/static/scripts/main.js
+++ lvda/src/main/resources/static/scripts/main.js
@@ -3,3 +3,15 @@ let myImage = document.getElementById("githubImgId");
 myImage.onclick = function() {
     window.open("http://127.0.0.1:8443/oauth/render/github");
 }
+
+function loadWebAssembly(fileName) {
+    return fetch(fileName)
+        .then(response => response.arrayBuffer())
+        .then(bits => WebAssembly.compile(bits))
+        .then(module => { return new WebAssembly.Instance(module) });
+};
+
+loadWebAssembly('wasm/test.wasm').then(instance => {
+    squarer = instance.exports._Z5helloid;
+    console.log('Finished compiling! Ready when you are...');
+});
